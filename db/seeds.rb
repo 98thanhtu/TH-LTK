4a4a9b7e549bcb75f5857b40690eba31d9ed22a9
98thanhtu <98thanhtupham@gmail.com>
2021-08-29 09:43:32 +0700

    Authen Student Teacher

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Teacher.create!(
+    email: "admin@gmail.com",
+    password:              "0384424545",
+    password_confirmation: "0384424545")
+
+99.times do |n|
+    email = "hs#{n+1}@gmail.com"
+    password = "111111"
+    Student.create!(
+        email: email,
+        password:              password,
+        password_confirmation: password)
+end
