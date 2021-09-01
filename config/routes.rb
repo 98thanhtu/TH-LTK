a42d0a6e8bf9b5a7c37c253cba7bec2ff421b5be
98thanhtu <98thanhtupham@gmail.com>
2021-09-01 14:00:00 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/contact'
   namespace :students do
-    get 'reviews/show'
-    get 'reviews/index'
-  end
-  devise_for :admins
-  namespace :students do
     resources :notis, only: %w[index show]
     resources :reviews, only: %w[index show]
   end
@@ -14,6 +9,8 @@ Rails.application.routes.draw do
     resources :notis
     resources :reviews
   end
+  
+  devise_for :admins
   devise_for :teachers
   devise_for :students
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
