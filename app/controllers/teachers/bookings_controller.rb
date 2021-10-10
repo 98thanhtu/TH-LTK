11d206a0027bcff86a71e06858709d8b7aa36455
98thanhtu <98thanhtupham@gmail.com>
2021-10-10 09:52:57 +0700

    fix header

--- app/controllers/teachers/bookings_controller.rb
+++ app/controllers/teachers/bookings_controller.rb
@@ -19,15 +19,18 @@ class Teachers::BookingsController < ApplicationController
 
   def edit
     bookings = current_teacher.bookings
-    @booking = bookings.find(params[:id])
+    @booking = bookings.find_by(id: params[:id])
   end
 
   def update
     bookings = current_teacher.bookings
-    @booking = bookings.find(params[:id])
+    @booking = bookings.find_by(id: params[:id])
+    crt_student = @booking.student
+    crt_student.avg_mark += @booking.product.price
     @booking.status = 4
     respond_to do |format|
       if @booking.update(booking_params)
+        crt_student.save
         format.html { redirect_to teachers_bookings_path, notice: "Hủy yêu cầu mua hàng thành công." }
       else
         format.html { render :edit, status: :unprocessable_entity }
