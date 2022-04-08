5b53e40b31ee1bcc7f83cebca5e035ca406086b5
98thanhtu <98thanhtupham@gmail.com>
2022-04-08 23:33:08 +0700

    update file seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,157 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+Teacher.destroy_all
+Student.destroy_all
 
 Admin.create!(
-    email: "admin@gmail.com",
-    password:              "1ouaAALtlWWSSGQt8sjuI3poNpJLrNOro",
-    password_confirmation: "1ouaAALtlWWSSGQt8sjuI3poNpJLrNOro")
+  email: "admin@gmail.com",
+  password:              "1ouaAALtlWWSSGQt8sjuI3poNpJLrNOro",
+  password_confirmation: "1ouaAALtlWWSSGQt8sjuI3poNpJLrNOro")
 
 Teacher.create!(
-    email: "minhchautran.ltk@gmail.com",
-    password:              "123456",
-    password_confirmation: "123456")
+  email: "minhchau@gmail.com",
+  password:              "123456",
+  password_confirmation: "123456")
 
-36.times do |n|
-    email = "hs#{n+1}@gmail.com"
-    password = "111111"
-    teacher_id = 1
-    Student.create!(
-        name: Faker::Name.name,
-        teacher_id: teacher_id,
-        email: email,
-        password:              password,
-        password_confirmation: password)
-end
+Student.create!(
+  email: "lhban@gmail.com",
+  password: "zBPGMr4i",
+  password_confirmation: "zBPGMr4i", 
+)
+Student.create!(
+  email: "ctpanh@gmail.com",
+  password: "i6ciYnJq",
+  password_confirmation: "i6ciYnJq",
+)
+Student.create!(
+  email: "ttkanh@gmail.com",
+  password: "Q04k3VBu",
+  password_confirmation: "Q04k3VBu",
+)
+Student.create!(
+  email: "ntbinh@gmail.com",
+  password: "BBB550uE",
+  password_confirmation: "BBB550uE",
+)
+Student.create!(
+  email: "ttqchi@gmail.com",
+  password: "oM3pOzRt",
+  password_confirmation: "oM3pOzRt",
+)
+Student.create!(
+  email: "ptmhao@gmail.com",
+  password: "DXuH28xp",
+  password_confirmation: "DXuH28xp",
+)
+Student.create!(
+  email: "tqhoang@gmail.com",
+  password: "xKK22t4h",
+  password_confirmation: "xKK22t4h",
+)
+Student.create!(
+  email: "ltmhuy@gmail.com",
+  password: "LJjl15cC",
+  password_confirmation: "LJjl15cC",
+)
+Student.create!(
+  email: "tnghuy@gmail.com",
+  password: "b1WRgUwH",
+  password_confirmation: "b1WRgUwH",
+)
+Student.create!(
+  email: "pbakiet@gmail.com",
+  password: "wap38vUv",
+  password_confirmation: "wap38vUv",
+)
+Student.create!(
+  email: "lhnminh@gmail.com",
+  password: "2DbCJ8wx",
+  password_confirmation: "2DbCJ8wx",
+)
+Student.create!(
+  email: "plminh@gmail.com",
+  password: "5MtPDdjp",
+  password_confirmation: "5MtPDdjp",
+)
+Student.create!(
+  email: "pttnga@gmail.com",
+  password: "jOuQ4Qbi",
+  password_confirmation: "jOuQ4Qbi",
+)
+Student.create!(
+  email: "hbngoc@gmail.com",
+  password: "N8FCu3hy",
+  password_confirmation: "N8FCu3hy",
+)
+Student.create!(
+  email: "nntngoc@gmail.com",
+  password: "RTTJ7QvJ",
+  password_confirmation: "RTTJ7QvJ",
+)
+Student.create!(
+  email: "tdhnhan@gmail.com",
+  password: "L2128WjJ",
+  password_confirmation: "L2128WjJ",
+)
+Student.create!(
+  email: "dtynhi@gmail.com",
+  password: "NYdazV2P",
+  password_confirmation: "NYdazV2P",
+)
+Student.create!(
+  email: "lnmphuong@gmail.com",
+  password: "l1yIVsLM",
+  password_confirmation: "l1yIVsLM",
+)
+Student.create!(
+  email: "nvhquy@gmail.com",
+  password: "5CgR3pKn",
+  password_confirmation: "5CgR3pKn",
+)
+Student.create!(
+  email: "pqquy@gmail.com",
+  password: "yBF5Vh1I",
+  password_confirmation: "yBF5Vh1I",
+)
+Student.create!(
+  email: "ldtthanh@gmail.com",
+  password: "i11kR8sJ",
+  password_confirmation: "i11kR8sJ",
+)
+Student.create!(
+  email: "tntien@gmail.com",
+  password: "dD9smZ8H",
+  password_confirmation: "dD9smZ8H",
+)
+Student.create!(
+  email: "phptran@gmail.com",
+  password: "4o0fIGJy",
+  password_confirmation: "4o0fIGJy",
+)
+Student.create!(
+  email: "ttnuyen@gmail.com",
+  password: "TWWvs15U",
+  password_confirmation: "TWWvs15U",
+)
+Student.create!(
+  email: "ndkvan@gmail.com",
+  password: "mTFu4IOy",
+  password_confirmation: "mTFu4IOy",
+)
+Student.create!(
+  email: "tnuvu@gmail.com",
+  password: "8QrbJ1bs",
+  password_confirmation: "8QrbJ1bs",
+)
+Student.create!(
+  email: "mkvy@gmail.com",
+  password: "uN2Oomh8",
+  password_confirmation: "uN2Oomh8",
+)
+Student.create!(
+  email: "nkxuyen@gmail.com",
+  password: "gtg1yLly",
+  password_confirmation: "gtg1yLly",
+)
